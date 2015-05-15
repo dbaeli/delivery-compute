5d3fba5a032ff7940e8eaf5e9e7d8c8797a5eaeb
Dimitri <dbaeli@MBP-DBA.local>
2015-05-15 22:11:12 +0200

    Initial commit

---
+++ src/delivery_compute/deliverycompute/__init__.py
@@ -0,0 +1,22 @@
+'''
+delivery-compute: Main module
+
+Copyright 2015, dbaeli
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'deliverycompute' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
