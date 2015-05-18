528439075157cb0aaece11eb0d72eba5d51646d9
dbaeli <dbaeli@gmail.com>
2015-05-18 16:23:11 +0200

    Adding tests

--- src/delivery_compute/tests/basic_test.py
+++ src/delivery_compute/tests/basic_test.py
@@ -11,3 +11,15 @@ Licensed under MIT
 
 def test_example():
     assert True
+
+def test_example1():
+    assert True
+
+def test_example2():
+    assert True
+
+def test_example3():
+    assert True
+
+def test_example4():
+    assert True
